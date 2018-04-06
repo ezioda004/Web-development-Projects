76751343416427f812294e7acd4e3801123001cd
Aditya Verma <adi004.zz@gmail.com>
2018-04-06 23:52:50 +0530

    Started working on the project

--- FreeCodeCamp Camper Leaderboard/camper-leaderboard/src/App.js
+++ FreeCodeCamp Camper Leaderboard/camper-leaderboard/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './Header.js';
+import {Footer} from './Footer.js';
+import Leaderboard from './Leaderboard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Header />
+        <Leaderboard/>
+        <Footer />
       </div>
     );
   }
---
+++ FreeCodeCamp Camper Leaderboard/camper-leaderboard/src/Leaderboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Leaderboard extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div> This is leaderboard, test</div>
+        )
+    }
+}
